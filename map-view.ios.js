01765f73fba651ac027dbaf29b01814c5f6aae38
Ricardo Bocchi <ricardobocchi@gmail.com>
2016-05-24 15:55:32 -0300

    bug fix

--- map-view.ios.js
+++ map-view.ios.js
@@ -140,32 +140,17 @@ var MapView = (function (_super) {
 
     var bounds = GMSCoordinateBounds.alloc().init()
 
-
     for(var marker in MARKER_WINDOW_IMAGES){  
-
-      console.log("## marker=" + marker)
-      console.log("## marker=" + MARKER_WINDOW_IMAGES[marker].position)
-
       var position = MARKER_WINDOW_IMAGES[marker].position
-
-      bounds = bounds.includingCoordinate(position)
-      
+      bounds = bounds.includingCoordinate(position)    
     }
 
     var update = GMSCameraUpdate.fitBoundsWithPadding(bounds, 100.0)
-    //this._ios.animateWithCameraUpdate(update);
-    //this._ios.moveCamera(update);
-    //this._ios.animateToViewingAngle(50);
 
     if(centerMarker){
-      console.log("## center 1")
       var center = GMSCameraUpdate.setTargetZoom(centerMarker.position, this.zoom);
-      //var center = GMSCameraPosition.cameraWithLatitudeLongitudeZoom(centerMarker.position.latitude, centerMarker.position.longitude, this.zoom)
-      console.log("## center 2")
       this._ios.moveCamera(center)
-      console.log("## center 3")
       this._ios.animateWithCameraUpdate(update);  
-      console.log("## center 4")
     }else{
       this._ios.animateWithCameraUpdate(update);  
     }    
@@ -185,15 +170,15 @@ var MapView = (function (_super) {
     if(this.draggable == undefined || this.draggable == null)
       this.draggable = false
 
-    if(opts.latitude){
-      if(isNaN(opts.latitude) && opts.latitude.length > 16)
+    if(opts.latitude && isNaN(opts.latitude)){
+      if(opts.latitude.length > 16)
         opts.latitude = Number(opts.latitude.substring(0, 16))
       else
         opts.latitude = Number(opts.latitude);
     }
 
-    if(opts.longitude){
-      if(isNaN(opts.longitude) && opts.longitude.length > 16)
+    if(opts.longitude && isNaN(opts.longitude)){
+      if(opts.longitude.length > 16)
         opts.longitude = Number(opts.longitude.substring(0, 16))
       else
         opts.longitude = Number(opts.longitude);
@@ -228,11 +213,11 @@ var MapView = (function (_super) {
     if(opts.clear)
       this.clear()
 
-    var latLng = CLLocationCoordinate2DMake(this.latitude, this.longitude)//.takeRetainedValue();
+    var latLng = CLLocationCoordinate2DMake(opts.latitude, opts.longitude)//.takeRetainedValue();
     openedMarker = GMSMarker.alloc().init()
     openedMarker.position = latLng;    
-    openedMarker.title = this.title;
-    openedMarker.snippet = this.snippet;    
+    openedMarker.title = opts.title;
+    openedMarker.snippet = opts.snippet;    
     openedMarker.draggable = this.draggable;
     openedMarker.icon  = iconToUse;
 
@@ -523,6 +508,18 @@ var MapView = (function (_super) {
       return degrees * 3.14 / 180.0
   }
 
+  MapView.prototype.hasMarkerLocation = function(args){
+
+    for(var marker in MARKER_WINDOW_IMAGES){
+      var it = MARKER_WINDOW_IMAGES[marker].position
+      if(it.latitude == args.latitude && it.longitude == args.longitude)
+        return true
+    }    
+
+    return false
+
+  }  
+
   MapView.prototype.distance = function(params){
     // let's give those values meaningful variable names
 
@@ -720,7 +717,7 @@ var MapView = (function (_super) {
   }
 
   MapView.prototype.defaultWindowMarkerCreator = function(marker){
-
+    
     if(this._custonWindowMarkerCreator)
       return this._custonWindowMarkerCreator({
         'marker': marker,
@@ -737,20 +734,16 @@ var MapView = (function (_super) {
     else
       console.log('## not has image to custon window')
 
-
     var outerView = UIImageView.alloc().initWithFrame(CGRectMake(0, 0, 270, 155))
     outerView.contentMode = UIViewContentModeScaleToFill
     outerView.image = UIImage.imageNamed("bubble")
     //outerView.backgroundColor = UIColor.whiteColor()
-
 
     var title = UILabel.alloc().initWithFrame(CGRectMake(10, 10, 170, 10))
     title.font = UIFont.systemFontOfSize(14)
     title.text = marker.title
     title.textColor = UIColor.blueColor()
     outerView.addSubview(title)
-
-
 
     var snippet = UILabel.alloc().initWithFrame(CGRectMake(10, 30, 170, 10))
     snippet.font = UIFont.systemFontOfSize(12)
@@ -779,6 +772,7 @@ var MapView = (function (_super) {
     if(badge){              
       var image 
       if(badge.indexOf('res://') > -1){      
+        console.log("step 10")
         var resName = badge.substring('res://'.length, badge.length)
         console.log("#### resName=" + resName)
         image = UIImage.imageNamed(resName)
@@ -786,13 +780,12 @@ var MapView = (function (_super) {
         image = UIImage.imageWithContentsOfFile(badge);
       }
 
-      console.log("## image=" + image)
-
       var imageView = UIImageView.alloc().initWithImage(image)
-      imageView.frame = CGRectMake(170, 10, 100, 80)
+      imageView.frame = CGRectMake(170, 5, 90, 80)
       imageView.contentMode = UIViewContentModeScaleAspectFit
       outerView.addSubview(imageView)
     }
+
 
     return outerView
   }  
@@ -801,4 +794,4 @@ var MapView = (function (_super) {
 })(common.MapView);
 
 
-exports.MapView = MapView;
+exports.MapView = MapView;  
